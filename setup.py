3e11818e21a893ec7b194c5add0d0b2dae7beb84
Ewen Lawrence <ewenlawrence@gmail.com>
2024-04-26 12:19:19 +0100

    Update to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,33 @@
 from setuptools import find_packages, setup
 
-# Update when actually setting up in 
-setup(
-    name='qu-magpy',
-    packages=find_packages(include=['magpy']),
-    version='0.1.0',
-    description='A python 3 library for computing the Adiabatic Gauge Potential',
-    author='Ewen Lawrence',
-    install_requires=['numpy>=1.26.3'],
-    tests_require=[],
-)
+from magpy.version import __version__
+
+import os
+import sys
+
+# Update path with current directory
+curr_dir = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, curr_dir)
+
+# set the descriptions
+short_desc = "A python 3 library for computing the Adiabatic Gauge Potential"
+with open(os.path.join(curr_dir, "README.md"), "r") as f:
+    long_description = f.read()
+
+# load requirements list
+with open(os.path.join(curr_dir, "requirements.txt"), "r") as requirements_file:
+    requirements = requirements_file.readlines()
+
+#TODO add keywords
+setup(name='qu-magpy',
+      version=__version__,
+      description=short_desc,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Ewen Lawrence',
+      author_email='ewenlawrence@gmail.com',
+      url='https://github.com/ewenlawrence/mAGPy',
+      license="Apache 2.0",
+      packages=find_packages(include=['magpy']),
+      install_requires=requirements,
+      python_requires=('>=3.9.0'))
